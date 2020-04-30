4acb3ec47cba7715fcfca70206a44f1becb119d4
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-30 21:31:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,8 +8,9 @@ def deal_card
 end
 
 def display_card_total(card_total)
-  # puts "Your cards add up to #{card_total}"
+  puts "Your cards add up to #{card_total}"
   # card_total
+  
 end
 
 def prompt_user
